2191ccf74f23e97a37433671e318e39b9adf0975
JayanthMenon <22003801@myrp.edu.sg>
2025-02-14 05:11:20 +0800

    deleted yaml, back to original

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.use((req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`\n✅ Server running on http://localhost:${PORT}`);
 });
